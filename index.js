f4f399edaa9d2da563fa6b725064ffffbbdfd5d6
fuadhasan199 <fuadhasan740266@gmail.com>
2025-08-29 02:17:14 +0600

    call history and call button

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const callButtons=document.getElementsByClassName("call-btn")
 const callHistory=document.getElementById("call-history") 
 
 const navCoin=document.getElementById("nav-coin") 
+const clearBtn=document.getElementById("clear-Btn")
 
 for (const callbutton of callButtons) {
    
@@ -34,19 +35,41 @@ for (const callbutton of callButtons) {
      if( coins < 0) coins=0 
      navCoin.textContent=coins 
 
-     const title=callbutton.getAttribute("data-title")
-     const number=callbutton.getAttribute("data-num") 
+     const title=callbutton.getAttribute('data-title')
+     const number=callbutton.getAttribute('data-num') 
+     
+      
      
      alert(`Calling ${title}: ${number}`) 
+ 
+     const addTime=document.createElement("span")
+     const date=new Date() 
+     addTime.textContent=date.toLocaleTimeString() 
+
+
+     clearBtn.onclick=function(){
+  
+          const entries = Array.from(callHistory.children).slice(1); 
+
+          for (const entrie of entries) {
+             entrie.remove()
+             
+          }
+
+
+     }
 
 
 const historyAdd = document.createElement("div");
 
-historyAdd.className = "border-b p-2 flex flex-col";
+historyAdd.className = " bg-gray-100 text-gray-800 mt-5"; 
+addTime.className="bg-gray-100 text-gray-800 "
 
 historyAdd.textContent = ` ${title} : ${number} `;
 
 callHistory.appendChild(historyAdd);
+
+callHistory.appendChild(addTime)
 
 
 
